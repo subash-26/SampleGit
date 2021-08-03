5a53c1447ae773bed1efccb250fba85f1a4367a0
subash-26 <rsubash122@gmail.com>
2021-08-03 14:16:22 +0530

    hi

--- src/org/login/Login.java
+++ src/org/login/Login.java
@@ -2,10 +2,16 @@ package org.login;
 
 public class Login {
 
-	public void A() {
+	public static void A() {
 		
 		System.out.println("hi hello");
 		System.out.println("Enjoy");
-		
 	}
-}
+	
+		
+		public static void  main(String[] args)
+		{
+			A();
+		}
+	}
+
